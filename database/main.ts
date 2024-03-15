5147fa7dc96d00a2021f549284d3d4979b97d969
caio santos <caio.santos687@gmail.com>
2024-03-14 23:44:08 -0300

    config ssm

--- database/main.ts
+++ database/main.ts
@@ -2,6 +2,7 @@
   import { App, TerraformOutput, TerraformStack } from "cdktf";
   import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
   import { DbInstance } from "@cdktf/provider-aws/lib/db-instance";
+  import { SsmParameter } from "@cdktf/provider-aws/lib/ssm-parameter";
 
   class MyStack extends TerraformStack {
     constructor(scope: Construct, id: string) {
@@ -9,7 +10,7 @@
 
       // define provider
       new AwsProvider(this, "aws", {
-        region: "us-west-1",
+        region: process.env.AWS_DEFAULT_REGION,
       });
 
 
@@ -21,10 +22,33 @@
         identifier: "lanchonete-db",
         instanceClass: "db.t3.micro",
         dbName: "lanchonetedatabase",
-        username: "myuser",
-        password: "mypassword",
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
         skipFinalSnapshot: true,
-        vpcSecurityGroupIds: ["sg-0baf026fe6c20e3f2"],
+        vpcSecurityGroupIds: ["sg-035a195544747abcc"],
+      });
+
+
+      // Save Database user
+      new SsmParameter(this, "dbUser", {
+        name: "/database/user",
+        type: "String",
+        value: process.env.DB_USER,
+      });
+
+      // Save Database address with port
+      new SsmParameter(this, "dbAddress", {
+        name: "/database/address",
+        type: "String",
+        value: dbInstance.endpoint,
+      });
+
+
+      // Save Database password
+      new SsmParameter(this, "dbPassword", {
+        name: "/database/password",
+        type: "SecureString",
+        value: process.env.DB_PASSWORD,
       });
       
   
